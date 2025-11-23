d47789961cd8472b71954ff4853733a5836ce2e4
Bahaa973 <bahaamarstawe@gmail.com>
2025-11-23 19:30:57 +0300

    refactor navigation in our app

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,7 @@
-import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -54,9 +53,10 @@ class _SplashViewBodyState extends State<SplashViewBody>
     Future.delayed(
       const Duration(seconds: 3),
       () {
-        Get.to(() => const HomeView(),
-            transition: Transition.rightToLeftWithFade,
-            duration: kTransitionDuration);
+        // Get.to(() => const HomeView(),
+        //     transition: Transition.rightToLeftWithFade,
+        //     duration: kTransitionDuration);
+        GoRouter.of(context).push(AppRouter.kHomeView);
       },
     );
   }
