f53b6794abea5de42a5f8fd19ba4f6b3771d04f4
Bahaa973 <bahaamarstawe@gmail.com>
2025-11-24 17:20:11 +0300

    add rating to book details

--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -2,14 +2,19 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+
+  final MainAxisAlignment mainAxisAlignment;
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           Icons.star,
+          size: 14,
           color: Color(0xffFFDD4F),
         ),
         const SizedBox(
@@ -22,10 +27,13 @@ class BookRating extends StatelessWidget {
         const SizedBox(
           width: 5,
         ),
-        Text(
-          '(254)',
-          style: Styles.textStyle14.copyWith(
-            color: const Color(0xff707070),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(254)',
+            style: Styles.textStyle14.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
           ),
         )
       ],
